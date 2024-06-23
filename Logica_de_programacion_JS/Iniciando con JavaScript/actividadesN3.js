80344fcf0b0cbd8a58b0dfe81f0c20922e44c367
Derlix <christianariasloaiza69@gmail.com>
2024-06-23 15:20:08 -0500

    Completando Loops y Tentativas

---
+++ Logica_de_programacion_JS/Iniciando con JavaScript/actividadesN3.js
@@ -0,0 +1,36 @@
+// Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+
+let contador = 1;
+
+while(contador <= 10){
+    alert(contador);
+    contador++;
+}
+
+// Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
+let contador00 = 10;
+
+while(contador00>= 0){
+    alert(contador00);
+
+    contador00--;
+}
+
+
+// Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+let limite = prompt("Ingresa el limite");
+let nuevoContador = 0;
+
+while(nuevoContador <= limite){
+    console.log(nuevoContador);
+    nuevoContador++;
+}
+
+// Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+let limite1 = prompt("Ingresa el limite");
+let nuevoContador1 = 0;
+
+while(nuevoContador1 <= limite1){
+    console.log(nuevoContador1);
+    nuevoContador++;
+}
