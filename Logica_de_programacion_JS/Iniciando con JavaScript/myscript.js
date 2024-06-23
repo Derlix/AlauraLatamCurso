a05535ba3fd0295192a401705d0a31b36cd12c44
Derlix <christianariasloaiza69@gmail.com>
2024-06-23 18:58:14 -0500

    Desafio

--- Logica_de_programacion_JS/Iniciando con JavaScript/myscript.js
+++ Logica_de_programacion_JS/Iniciando con JavaScript/myscript.js
@@ -1,20 +1,31 @@
 //Variables
+let numeroLimite = 0;
 
-let numeroSecreto = Math.floor(Math.random()*10)+1;
+//Solicitamos el numero limite al usuario
+while(numeroLimite <= 0){
+    numeroLimite = parseInt(prompt("Ingresa hasta qué número quieres jugar desde el 1 en adelante"));
+    if(numeroLimite <= 0){
+        alert("Ingresa un número mayor a 0");
+    }
+}
+
+let numeroSecreto = Math.floor(Math.random()*numeroLimite)+1;
 let contador = 1;
 let numeroDeUsuario = 0;
 //let palabraVeces = 'intento';
 let maximosIntentos = 10;
 /* Este codigo realiza la comparacion*/
-let intentos
+let intentos;
+
+console.log(numeroSecreto);
 while(numeroSecreto != numeroDeUsuario){
-    numeroDeUsuario = parseInt(prompt("Ingresa un numero por favor: "))
+    numeroDeUsuario = parseInt(prompt(`Ingresa un numero entre el 1 hasta el:${numeroLimite} `))
 
     console.log("Numero ingresado:" + numeroDeUsuario);
 
     if (numeroDeUsuario == numeroSecreto){
         //Si entro por aca significa que acertamos y la condicion 
-        alert(`Acertaste, el numero es: ${numeroSecreto} en: ${contador}: ${intentos == 1 ? 'intento' : 'intentos'}`);
+        alert(`Acertaste, el numero es: ${numeroSecreto} en: ${contador}  ${intentos == 1 ? 'intento' : 'intentos'}`);
         console.log("ganaste")
     }else{
         if(numeroDeUsuario > numeroSecreto){
