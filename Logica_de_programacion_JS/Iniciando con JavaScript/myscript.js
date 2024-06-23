4d3fb4c1b2876646406dbc84b4a7f71f30824555
Derlix <christianariasloaiza69@gmail.com>
2024-06-23 18:33:42 -0500

    Buenas Practicas en Programacion

--- Logica_de_programacion_JS/Iniciando con JavaScript/myscript.js
+++ Logica_de_programacion_JS/Iniciando con JavaScript/myscript.js
@@ -1,29 +1,34 @@
 //Variables
 
-let numeroSecreto = 5;
+let numeroSecreto = Math.floor(Math.random()*10)+1;
 let contador = 1;
 let numeroDeUsuario = 0;
-let palabraVeces = 'intento';
-
+//let palabraVeces = 'intento';
+let maximosIntentos = 10;
 /* Este codigo realiza la comparacion*/
-
+let intentos
 while(numeroSecreto != numeroDeUsuario){
-    numeroDeUsuario = prompt("Ingresa un numero por favor: ");
+    numeroDeUsuario = parseInt(prompt("Ingresa un numero por favor: "))
 
     console.log("Numero ingresado:" + numeroDeUsuario);
 
     if (numeroDeUsuario == numeroSecreto){
         //Si entro por aca significa que acertamos y la condicion 
-        alert(`Acertaste, el numero es: ${numeroDeUsuario} ${palabraVeces}: ${contador}`);
+        alert(`Acertaste, el numero es: ${numeroSecreto} en: ${contador}: ${intentos == 1 ? 'intento' : 'intentos'}`);
+        console.log("ganaste")
     }else{
         if(numeroDeUsuario > numeroSecreto){
             alert('El numero secreto es menor');
         }else{
             alert('El numero secreto es mayor');
         }
-        palabraVeces = 'intentos';
         //Incrementamos el contador cuando no acierta
         contador = contador + 1;
+        //palabraVeces = 'intentos';
+        if(contador>maximosIntentos){
+            alert(`Llegaste al numero maximo de intentos ${maximosIntentos}`);
+            break;
+        }
         //si entro por aca significa que la condicion no se cumplio 
         //alert('No acertaste el numero')
     }
